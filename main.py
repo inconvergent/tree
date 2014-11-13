62420f90c6938e7a5e0924f1b43e1a1c6cf35579
anders hoff <shall.be.lifted@gmail.com>
2014-11-14 00:42:31 +0100

    .

--- main.py
+++ main.py
@@ -4,7 +4,6 @@
 from numpy import pi
 
 
-FNAME = './img/xx'
 NMAX = int(2*1e8)
 
 SIZE = 10000
@@ -16,18 +15,18 @@ MID = 0.5
 
 
 INIT_BRANCH = SIZE*0.03*ONE
-GRAINS = int(SIZE*0.03)
+GRAINS = int(SIZE*0.04)
 
 BRANCH_DIMINISH = ONE/27.
 BRANCH_SPLIT_DIMINISH = 0.71
 BRANCH_SPLIT_ANGLE = 0.3*pi
-BRANCH_PROB_SCALE = 1./(INIT_BRANCH)/SIZE*40.
+BRANCH_PROB_SCALE = 1./(INIT_BRANCH)/SIZE*42.
 
 BRANCH_ANGLE_MAX = 10.*pi/SIZE
 BRANCH_ANGLE_EXP = 2.2
 
 ## COLORS AND SHADES
-BACK = [1,1,1,1]
+BACK = [1,1,1,0]
 FRONT = [0, 0, 0, 0.5]
 TRUNK_STROKE = [0, 0, 0, 1]
 TRUNK = [1, 1, 1, 1]
@@ -41,37 +40,37 @@ def main():
   from tree import Tree
   from time import time
 
-  render = Render(SIZE, FRONT, BACK, TRUNK, TRUNK_STROKE,GRAINS)
-  render.ctx.set_source_rgba(*FRONT)
-  render.ctx.set_line_width(ONE)
 
-  #while True:
-  #render.clear_canvas()
+  while True:
 
-  tree = Tree(MID,
-              0.9,
-              INIT_BRANCH,
-              -pi*0.5,
-              ONE,
-              ONE,
-              BRANCH_SPLIT_ANGLE,
-              BRANCH_PROB_SCALE,
-              BRANCH_DIMINISH,
-              BRANCH_SPLIT_DIMINISH,
-              BRANCH_ANGLE_MAX,
-              BRANCH_ANGLE_EXP)
+    render = Render(SIZE, FRONT, BACK, TRUNK, TRUNK_STROKE,GRAINS)
+    render.ctx.set_source_rgba(*FRONT)
+    render.ctx.set_line_width(ONE)
 
-  i = 1
-  while tree.Q:
+    tree = Tree(MID,
+                0.9,
+                INIT_BRANCH,
+                -pi*0.5,
+                ONE,
+                ONE,
+                BRANCH_SPLIT_ANGLE,
+                BRANCH_PROB_SCALE,
+                BRANCH_DIMINISH,
+                BRANCH_SPLIT_DIMINISH,
+                BRANCH_ANGLE_MAX,
+                BRANCH_ANGLE_EXP)
 
-    i += 1
-    tree.step()
-    map(render.branch2,tree.Q)
+    i = 1
+    while tree.Q:
 
-    if not i%1000:
-      print i
+      i += 1
+      tree.step()
+      map(render.branch2,tree.Q)
 
-  render.sur.write_to_png('./img/test_{:10.0f}.png'.format(time()))
+      if not i%1000:
+        print i, len(tree.Q)
+
+    render.sur.write_to_png('./img/test_42_3_{:10.0f}.png'.format(time()))
 
 
 if __name__ == '__main__':
