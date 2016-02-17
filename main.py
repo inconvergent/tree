9043d00801fc0783960b5fc6a9f06ade42e3f933
Anders Hoff <inconvergent@gmail.com>
2016-02-17 20:39:24 +0100

    restructure

--- main.py
+++ main.py
@@ -16,18 +16,18 @@ MID = 0.5
 
 
 INIT_BRANCH = SIZE*0.03*ONE
-GRAINS = int(SIZE*0.03)
+GRAINS = int(SIZE*0.04)
 
 BRANCH_DIMINISH = ONE/27.
 BRANCH_SPLIT_DIMINISH = 0.71
 BRANCH_SPLIT_ANGLE = 0.3*pi
-BRANCH_PROB_SCALE = 1./(INIT_BRANCH)/SIZE*40.
+BRANCH_PROB_SCALE = 1./(INIT_BRANCH)/SIZE*42
 
 BRANCH_ANGLE_MAX = 10.*pi/SIZE
 BRANCH_ANGLE_EXP = 2.2
 
 ## COLORS AND SHADES
-BACK = [1,1,1,1]
+BACK = [1,1,1,0]
 FRONT = [0, 0, 0, 0.5]
 TRUNK_STROKE = [0, 0, 0, 1]
 TRUNK = [1, 1, 1, 1]
@@ -37,29 +37,33 @@ LEAF = [0,0,1,0.5]
 
 def main():
 
-  from render import Render
-  from tree import Tree
+  from modules.render import Render
+  from modules.tree import Tree
   from time import time
-
-  render = Render(SIZE, FRONT, BACK, TRUNK, TRUNK_STROKE,GRAINS)
-  render.ctx.set_source_rgba(*FRONT)
-  render.ctx.set_line_width(ONE)
 
   while True:
 
-    render.clear_canvas()
-    tree = Tree(MID,
-                0.9,
-                INIT_BRANCH,
-                -pi*0.5,
-                ONE,
-                ONE,
-                BRANCH_SPLIT_ANGLE,
-                BRANCH_PROB_SCALE,
-                BRANCH_DIMINISH,
-                BRANCH_SPLIT_DIMINISH,
-                BRANCH_ANGLE_MAX,
-                BRANCH_ANGLE_EXP)
+    render = Render(SIZE, FRONT, BACK, TRUNK, TRUNK_STROKE,GRAINS)
+    render.ctx.set_source_rgba(*FRONT)
+    render.ctx.set_line_width(ONE)
+
+    # TODO :does not work with transparent bg. use operators
+    #render.clear_canvas()
+
+    tree = Tree(
+      MID,
+      0.9,
+      INIT_BRANCH,
+      -pi*0.5,
+      ONE,
+      ONE,
+      BRANCH_SPLIT_ANGLE,
+      BRANCH_PROB_SCALE,
+      BRANCH_DIMINISH,
+      BRANCH_SPLIT_DIMINISH,
+      BRANCH_ANGLE_MAX,
+      BRANCH_ANGLE_EXP
+    )
 
     i = 1
     while tree.Q:
@@ -68,10 +72,10 @@ def main():
       tree.step()
       map(render.branch2,tree.Q)
 
-      if not i%1000:
-        print i
+      if not i%200:
+        print i, len(tree.Q)
 
-    render.sur.write_to_png('/media/var/andreas_tree/test_52_{:10.0f}.png'.format(time()))
+    render.sur.write_to_png('/media/var/andreas_tree/test_bp42_{:10.0f}.png'.format(time()))
 
 if __name__ == '__main__':
 
--- main_ani.py
+++ main_ani.py
@@ -4,7 +4,7 @@
 from numpy import pi
 
 
-SIZE = 1080
+SIZE = 560
 ONE = 1./SIZE
 
 
@@ -33,41 +33,56 @@ LEAF = [0,0,1,0.5]
 
 STEPS_ITT = 1
 
+i = 0
+
 
 def main():
 
-  from render import Animate
-  from tree import Tree
+  from modules.render import Animate
+  from modules.tree import Tree
   import gtk
 
-  tree = Tree(MID,
-              0.95,
-              INIT_BRANCH,
-              -pi*0.5,
-              ONE,
-              ONE,
-              BRANCH_SPLIT_ANGLE,
-              BRANCH_PROB_SCALE,
-              BRANCH_DIMINISH,
-              BRANCH_SPLIT_DIMINISH,
-              BRANCH_ANGLE_MAX,
-              BRANCH_ANGLE_EXP)
+  tree = Tree(
+    MID,
+    0.95,
+    INIT_BRANCH,
+    -pi*0.5,
+    ONE,
+    ONE,
+    BRANCH_SPLIT_ANGLE,
+    BRANCH_PROB_SCALE,
+    BRANCH_DIMINISH,
+    BRANCH_SPLIT_DIMINISH,
+    BRANCH_ANGLE_MAX,
+    BRANCH_ANGLE_EXP
+  )
 
   def wrap(steps_itt,render):
+
+    global i
 
     tree.step()
     map(render.branch2,tree.Q)
 
     if tree.Q:
+      render.sur.write_to_png('{:05d}.png'.format(i))
+      i += 1
       return True
     else:
-      tree.init()
-      render.clear_canvas()
-      return True
+      #tree.init()
+      #render.clear_canvas()
+      return False
 
 
-  render = Animate(SIZE, FRONT, BACK, TRUNK, TRUNK_STROKE,GRAINS,
-                   STEPS_ITT, wrap)
+  render = Animate(
+    SIZE,
+    FRONT,
+    BACK,
+    TRUNK,
+    TRUNK_STROKE,
+    GRAINS,
+    STEPS_ITT, wrap
+  )
   render.ctx.set_source_rgba(*FRONT)
   render.ctx.set_line_width(ONE)
 
